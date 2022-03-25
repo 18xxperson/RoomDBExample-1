7f38538bdd35381c8669d45de0692f509e367da9
18xxperson <46502267+18xxperson@users.noreply.github.com>
2022-03-24 23:20:21 -0700

    Update MainActivity.kt

--- app/src/main/java/com/revature/roomdbexample/MainActivity.kt
+++ app/src/main/java/com/revature/roomdbexample/MainActivity.kt
@@ -19,7 +19,6 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         val customerViewModel=ViewModelProvider(this).get(CustomerViewModel::class.java)
 
         setContent {
